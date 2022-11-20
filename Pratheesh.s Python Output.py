32913e46634beca6f7528c4348b3e2df549441a2
Pratheesh07 <118626255+Pratheesh07@users.noreply.github.com>
2022-11-20 09:29:12 +0530

    Add files via upload

---
+++ Pratheesh.s Python Output.py
@@ -0,0 +1,21 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:\Users\PRATHEESH\Desktop\Pratheesh.s  -python.py =========
+1
+Enter your Username:Lenovo
+Enter your Password:Thinkpad15
+wrong credentials
+2
+enter the value to check whether it is odd or even58
+It is even number
+3
+Mulberry
+Raspberry
+Watermelon
+Mandarin
+Pineapple
+Kiwi
+Fig
+Mango
+Banana
